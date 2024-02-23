0faf8763960ab7d6002843fa4b7232d3f6b13469
Kenneth Gray <mainfolder@Kenneths-Air.apsu.edu>
2024-02-23 13:07:24 -0600

    added requirements

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
