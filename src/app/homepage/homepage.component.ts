3c3d8e3784a04ad86dfc8e93ab923742c6eaa956
Kenneth Gray <mainfolder@Kenneths-Air.apsu.edu>
2024-02-28 13:23:28 -0600

    Finished homepage,profile

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,13 +3,14 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { LogInComponent } from '../log-in/log-in.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
