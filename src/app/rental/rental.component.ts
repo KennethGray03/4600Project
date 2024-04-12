0bc28c7c32033ff4cd7c34755abe7f9a9d6086e6
blue-canyon <bhicks6300@gmail.com>
2024-04-12 12:05:03 -0500

    search for trips&rental

--- src/app/rental/rental.component.ts
+++ src/app/rental/rental.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { Router } from '@angular/router';
 import { CartService } from '../cartService';
 import { WishlistService } from '../wishlist.service';
@@ -10,12 +10,12 @@ import { WishlistService } from '../wishlist.service';
   styleUrl: './rental.component.css'
 })
 export class RentalComponent  {
+  @ViewChild('searchBox') searchBox!: ElementRef;
   constructor(private router: Router, 
     private cartService: CartService,
     private wishlistService: WishlistService) { }
 
-ngOnInit(): void {
-}
+
 
 addToCart(product: any) {
 this.cartService.addToCart(product); // Add the product to the cart
@@ -26,5 +26,30 @@ addToWishlist(product: any){
 this.wishlistService.addToWishlist(product); 
 this.router.navigate(['/wishlist']); // Navigate to the cart page
 
+}
+
+highlightWord(): void {
+  let searchText = (this.searchBox.nativeElement as HTMLInputElement)?.value;
+  let elements = document.querySelectorAll("#content p, #content h2, #content span, #content button");
+
+  if (searchText) {
+      elements.forEach(element => {
+          let originalText = element.textContent  || '';
+          let escapedSearchText = searchText.replace(/[-/^$*+?.()|[]{}]/g, '$&'); // Escape special characters
+          let highlightedText = originalText.replace(new RegExp(escapedSearchText, "gi"), match => `<span style='background-color: yellow'>${match}</span>`);
+          element.innerHTML = highlightedText;
+      });
+  } else {
+      elements.forEach(element => {
+          element.innerHTML = element.textContent  ||'';
+      });
+  }
+ }
+ ngAfterViewInit() {
+  this.searchBox.nativeElement.addEventListener('keypress', (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      this.highlightWord();
+    }
+  });
 }
 }
