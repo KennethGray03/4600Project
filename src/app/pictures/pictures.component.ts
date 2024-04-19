cead92357b2623f1772ae1b239e7b7b85b931e1d
blue-canyon <bhicks6300@gmail.com>
2024-04-18 22:01:23 -0500

    add and format pics

--- src/app/pictures/pictures.component.ts
+++ src/app/pictures/pictures.component.ts
@@ -14,8 +14,16 @@ export class PicturesComponent implements OnInit {
   images = [
     { src: 'assets/Backpack.jpg', alt: 'Image 1', visible: true },
     { src: 'assets/Caving.jpg', alt: 'Image 2', visible: false },
-    { src: 'assets/ClimbingCompetition.jpg', alt: 'Image 3', visible: false },
-    { src: 'assets/outdoorRockClimbing.jpg', alt: 'Image 2', visible: false }
+    { src: 'assets/indian gravepoint.jpg', alt: 'Image 3', visible: false },
+    { src: 'assets/outdoorRockClimbing.jpg', alt: 'Image 4', visible: false },
+    { src: 'assets/campus comp.jpg', alt: 'Image 5', visible: false },
+    { src: 'assets/dyno comp.jpg', alt: 'Image 6', visible: false },
+    { src: 'assets/FG water.jpg', alt: 'Image 7', visible: false },
+    { src: 'assets/Fiery Gizzard.jpg', alt: 'Image 8', visible: false },
+    { src: 'assets/foster falls.jpg', alt: 'Image 9', visible: false },
+    { src: 'assets/grand canyon.jpg', alt: 'Image 10', visible: false },
+    { src: 'assets/kings bluff.jpg', alt: 'Image 11', visible: false },
+    { src: 'assets/smokies.jpg', alt: 'Image 12', visible: false }
   ];
   currentIndex = 0;
 
